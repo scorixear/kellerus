a72b152f1db0c16396f60ac9f1efb66ca57bd2b5
Paul Keller <pkeller1@stud.hs-heilbronn.de>
2020-02-28 11:47:29 +0100

    updated template config

--- config_template.js
+++ config_template.js
@@ -1,6 +1,14 @@
 const token = 'your_token';
 const discord_url = 'https://discordapp.com/oauth2/authorize?client_id=client_id&scope=bot&permissions=2146958807'
 
+const repository = 'https://github.com/scorixear/kellerus';
+
+const bot_prefix = '!';
+const version = '0.0.1';
+
 module.exports = {
-    token: token
+    token: token,
+    prefix: bot_prefix,
+    repo: repository,
+    version: version,
 }
