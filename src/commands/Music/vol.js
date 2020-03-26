859fb6aa6564bd4ea46215fae21729e5fefac2a4
Paul Keller <pkeller1@stud.hs-heilbronn.de>
2020-03-26 20:52:18 +0100

    added volumn command

---
+++ src/commands/Music/vol.js
@@ -0,0 +1,47 @@
+import Command from './../command.js';
+import musicPlayer from '../../misc/musicPlayer.js';
+import messageHandler from '../../misc/messageHandler.js';
+export default class Vol extends Command {
+  constructor(category) {
+    super(category);
+    this.usage = 'vol <percent>';
+    this.command = 'vol';
+    this.description = 'Adjust the volume of the bot';
+    this.example = 'vol 25';
+  }
+
+  executeCommand(args, msg) {
+    try {
+      super.executeCommand(args, msg);
+    } catch (err) {
+      return;
+    }
+    if (args.length != 1 || parseInt(args[0]) === NaN) {
+      messageHandler.sendRichTextDefault({
+        msg: msg,
+        title: 'Error 💥',
+        description: `Invalid usage`,
+        categories: [{
+          title: 'Usage',
+          text: `\`${this.usage}\``,
+        }],
+      });
+      return;
+    }
+    const volume = parseInt(args[0]) / 100;
+    if (volume > 2 || volume <= 0) {
+      messageHandler.sendRichTextDefault({
+        msg: msg,
+        title: 'Error 💥',
+        description: 'Volume can only between `0`% and `200`%',
+      });
+      return;
+    }
+    musicPlayer.setVolume(msg.guild.id, volume);
+    messageHandler.sendRichTextDefault({
+      msg: msg,
+      title: 'Volume set',
+      description: `Volume was set to ${args[0]}%`,
+    });
+  }
+}
