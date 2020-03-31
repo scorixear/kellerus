890e5134ab26491655c4fa35cd33b31d0abffc7d
Paul Keller <pkeller1@stud.hs-heilbronn.de>
2020-03-31 12:51:53 +0200

    added handler js docs

--- src/misc/sqlHandler.js
+++ src/misc/sqlHandler.js
@@ -1,6 +1,8 @@
 /* eslint-disable max-len */
 import mariadb from 'mariadb';
 import config from './../config.js';
+// eslint-disable-next-line no-unused-vars
+import Discord from 'discord.js';
 
 const pool = mariadb.createPool({
   host: config.dbhost,
@@ -12,6 +14,9 @@ const pool = mariadb.createPool({
   connectionLimit: 5,
 });
 
+/**
+ * Initialized the Database
+ */
 async function initDB() {
   let conn;
   try {
@@ -26,6 +31,11 @@ async function initDB() {
   }
 }
 
+/**
+ * Returns the Honor Count for a given user
+ * @param {Discord.User} user
+ * @return {number}
+ */
 async function getHonorCount(user) {
   let conn;
   let honorCount;
@@ -41,6 +51,11 @@ async function getHonorCount(user) {
   return honorCount;
 }
 
+/**
+ * Adds one Honor to the users account
+ * @param {Discord.User} user
+ * @return {number} the updated honor count
+ */
 async function addHonorCount(user) {
   let conn;
   let honorCount;
@@ -63,6 +78,11 @@ async function addHonorCount(user) {
   return honorCount;
 }
 
+/**
+ * Creates a Queue table
+ * @param {number} serverid
+ * @param {mariadb.PoolConnection} conn
+ */
 async function createQueue(serverid, conn) {
   try {
     await conn.query(`CREATE TABLE \`ServerQueue_${serverid}\` (\`ID\` INT UNSIGNED AUTO_INCREMENT, \`url\` VARCHAR(255), \`title\` VARCHAR(255), PRIMARY KEY (\`ID\`))`);
@@ -73,6 +93,11 @@ async function createQueue(serverid, conn) {
   }
 }
 
+/**
+ * Retrieves the Queue
+ * @param {number} serverid
+ * @return {Array<{title: string, url: string}>}
+ */
 async function getQueue(serverid) {
   let conn;
   const queue = [];
@@ -105,7 +130,13 @@ async function getQueue(serverid) {
   return queue;
 }
 
-
+/**
+ * Queues on title
+ * @param {string} title
+ * @param {string} url
+ * @param {number} serverid
+ * @return {boolean} if it was successfull
+ */
 async function addQueue(title, url, serverid) {
   let conn;
   let success;
@@ -132,6 +163,11 @@ async function addQueue(title, url, serverid) {
   return success;
 }
 
+/**
+ * Cleares one queue
+ * @param {number} serverid
+ * @return {boolean} if successfull
+ */
 async function clearQueue(serverid) {
   let conn;
   let success;
