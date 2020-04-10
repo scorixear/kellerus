09d5d973bb1015c8156638502fa8378607948c6e
Paul Keller <pkeller1@stud.hs-heilbronn.de>
2020-04-10 17:04:13 +0200

    added double digit for date format

--- src/misc/dateFormatHandler.js
+++ src/misc/dateFormatHandler.js
@@ -4,7 +4,11 @@
  * @return {string}
  */
 function formatDate(date) {
-  return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}, ${date.getHours()}:${date.getMinutes()}`;
+  return `${date.getFullYear()}/${toDoubleDigit(date.getMonth() + 1)}/${toDoubleDigit(date.getDate())}, ${toDoubleDigit(date.getHours)()}:${toDoubleDigit(date.getMinutes())}`;
+}
+
+function toDoubleDigit(number) {
+  return number < 10 ? '0'+number : number;
 }
 
 export default {
