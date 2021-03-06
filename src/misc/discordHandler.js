97106ce8e84eb637e9f4a9867538c90b78840e31
Paul Keller <pkeller1@stud.hs-heilbronn.de>
2021-03-06 14:56:34 +0100

    added bot channels

--- src/misc/discordHandler.js
+++ src/misc/discordHandler.js
@@ -1,5 +1,40 @@
 import Discord from 'discord.js';
+import sqlHandler from './sqlHandler';
 
 const client = new Discord.Client();
+const tempChannels = [];
+
+client.on("voiceStateUpdate", async (newVoice, oldVoice) => {
+  let newUserChannel = oldVoice.channel;
+  let oldUserChannel = newVoice.channel;
+
+  if(oldUserChannel !== newUserChannel) {
+    if(oldUserChannel && tempChannels.includes(oldUserChannel)) {
+      if(oldUserChannel.members.array().length === 0) {
+        await oldUserChannel.delete();
+      }
+      
+    }
+    if (newUserChannel) {
+      const replacement = await sqlHandler.findChannel(newUserChannel.id);
+      if(replacement) {
+        let counter = 1;
+        while(newVoice.guild.channels.cache.find(channel => channel.name === replacement.replace('$', counter))) {
+          counter++; 
+        }
+  
+        const channel = await newVoice.guild.channels.create(replacement.replace('$', counter), 
+        {
+          type: 'voice', 
+          parent: newUserChannel.parent,
+          permissionOverwrites: newUserChannel.permissionOverwrites,
+          position: newUserChannel.position,
+        });
+        tempChannels.push(channel);
+        newVoice.setChannel(channel);
+      }
+    }
+  }
+});
 
 export default {client};
--- src/misc/sqlHandler.js
+++ src/misc/sqlHandler.js
@@ -24,6 +24,7 @@ async function initDB() {
     conn = await pool.getConnection();
     console.log('DB Connection established');
     await conn.query('CREATE TABLE IF NOT EXISTS `honor` (`user_id` VARCHAR(255), `val` INT, PRIMARY KEY (`user_id`))');
+    await conn.query('CREATE TABLE IF NOT EXISTS `channels` (`id` VARCHAR(255), `replacement` VARCHAR(255), PRIMARY KEY (`id`))');
   } catch (err) {
     throw err;
   } finally {
@@ -105,10 +106,69 @@ async function removeHonor(user) {
   return honorCount;
 }
 
+async function saveChannel(channelId, replacement) {
+  let conn;
+  let returnValue = true;
+  try {
+    conn = await pool.getConnection();
+    let rows = await conn.query(`SELECT id FROM channels WHERE \`id\` = ${conn.escape(channelId)}`);
+    if(rows && rows[0]) {
+      await conn.query(`UPDATE channels SET replacement = ${conn.escape(replacement)} WHERE \`id\` = ${conn.escape(channelId)}`);
+    } else {
+      await conn.query(`INSERT INTO channels (id, replacement) VALUES (${conn.escape(channelId)}, ${conn.escape(replacement)})`);
+    }
+  } catch (err) {
+    returnValue = false;
+    console.error(err);
+  } finally {
+    if(conn) await conn.end();
+  }
+  return returnValue;
+}
+
+async function removeChannel(channelId) {
+  let conn;
+  let returnValue = true;
+  try {
+    conn = await pool.getConnection();
+    await conn.query(`DELETE FROM channels WHERE id = ${pool.escape(channelId)}`);
+  } catch(err) {
+    returnValue = false;
+    console.error(err);
+  } finally {
+    if(conn) conn.end();
+  }
+  return returnValue;
+}
+
+/**
+ * 
+ * @param {String} channelId 
+ */
+async function findChannel(channelId) {
+  let conn;
+  let returnValue;
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query(`SELECT replacement FROM channels WHERE id = ${pool.escape(channelId)}`);
+    if(rows && rows[0]) {
+      returnValue = rows[0].replacement;
+    }
+  } catch (err) {
+    throw err;
+  } finally {
+    if(conn) conn.end();
+  }
+  return returnValue;
+}
+
 export default {
   initDB,
   getHonorCount,
   addHonorCount,
   removeHonor,
+  saveChannel,
+  removeChannel,
+  findChannel,
   pool,
 };
